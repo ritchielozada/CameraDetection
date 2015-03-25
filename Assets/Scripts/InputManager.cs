908879a7a03b178b2c1677ac033cfe826c462e02
Ritchie Lozada <rlozada@microsoft.com>
2015-03-25 13:31:28 -0700

    CameraRig based controls
    Movement using Camera Rig, Pitch Control on Camera unit to maintain
    horizon lock.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,26 +4,32 @@ using System.Collections;
 public class InputManager : MonoBehaviour
 {
     float RotationDeltaRate = 90;        
-    private float pitch = 60;
+    private float camPitch = 60;
     private float yaw = 0;
-    private Quaternion orientation;
+    private Quaternion cameraOrientation;
+    private Quaternion rigOrientation;
+    private Camera camera;
+
+    void Start()
+    {
+        camera = GetComponentInChildren<Camera>();
+    }
        
     void Update()
     {
         float move_x = Input.GetAxis("HorizontalMove");
         float move_y = Input.GetAxis("VerticalMove");
-        float move_z = Input.GetAxis("ForwardMove");
-
-        pitch += Input.GetAxis("Vertical") * Time.deltaTime * RotationDeltaRate;
+        float move_z = Input.GetAxis("ForwardMove");        
         yaw -= Input.GetAxis("Horizontal") * Time.deltaTime * RotationDeltaRate;
+        camPitch += Input.GetAxis("Vertical") * Time.deltaTime * RotationDeltaRate;
                 
-        transform.Translate(Vector3.up * move_y + Vector3.right * move_x, Space.World);
+        transform.Translate(Vector3.up * move_y, Space.World);
+        transform.Translate(Vector3.forward * move_z + Vector3.right * move_x, Space.Self);  
         
-        // Change to Space.Self to support forward movement to forward vector
-        transform.Translate(Vector3.forward * move_z, Space.World);  
-
-        orientation.eulerAngles = new Vector3(LimitAngles(pitch), LimitAngles(yaw), 0);
-        transform.rotation = Quaternion.Lerp(transform.rotation, orientation, Time.time);     
+        rigOrientation.eulerAngles = new Vector3(0, LimitAngles(yaw), 0);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rigOrientation, Time.time);
+        cameraOrientation.eulerAngles = new Vector3(LimitAngles(camPitch), transform.rotation.eulerAngles.y, 0);
+        camera.transform.rotation = Quaternion.Lerp(camera.transform.rotation, cameraOrientation, Time.time);
     }
 
     float LimitAngles(float angle)
