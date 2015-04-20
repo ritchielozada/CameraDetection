84f8641346ce692cfb5a4145445b5c9a56268eb3
Ritchie Lozada <rlozada@outlook.com>
2015-04-19 20:25:36 -0700

    Meta Setup / Sync

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class InputManager : MonoBehaviour
 {
-    float RotationDeltaRate = 90;        
+    float RotationDeltaRate = 90;
     private float camPitch = 60;
     private float yaw = 0;
+    float move_x;
+    float move_y;
+    float move_z;
     private Quaternion cameraOrientation;
     private Quaternion rigOrientation;
     private Camera pivotCamera;
@@ -14,18 +17,21 @@ public class InputManager : MonoBehaviour
     {
         pivotCamera = GetComponentInChildren<Camera>();
     }
-       
+
     void Update()
     {
-        float move_x = Input.GetAxis("HorizontalMove");
-        float move_y = Input.GetAxis("VerticalMove");
-        float move_z = Input.GetAxis("ForwardMove");        
+        move_x = Input.GetAxis("HorizontalMove");
+        move_y = Input.GetAxis("VerticalMove");
+        move_z = Input.GetAxis("ForwardMove");
         yaw -= Input.GetAxis("Horizontal") * Time.deltaTime * RotationDeltaRate;
         camPitch += Input.GetAxis("Vertical") * Time.deltaTime * RotationDeltaRate;
-                
+    }
+
+    void FixedUpdate()
+    {
         transform.Translate(Vector3.up * move_y, Space.World);
-        transform.Translate(Vector3.forward * move_z + Vector3.right * move_x, Space.Self);  
-        
+        transform.Translate(Vector3.forward * move_z + Vector3.right * move_x, Space.Self);
+
         rigOrientation.eulerAngles = new Vector3(0, LimitAngles(yaw), 0);
         transform.rotation = Quaternion.Lerp(transform.rotation, rigOrientation, Time.time);
         cameraOrientation.eulerAngles = new Vector3(LimitAngles(camPitch), transform.rotation.eulerAngles.y, 0);
